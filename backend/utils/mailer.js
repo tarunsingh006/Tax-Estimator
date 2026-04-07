cf2e699510e22f8f834f5f5970e706074740a77f
tarunsingh006 <tarunchauhan8832@gmail.com>
2026-04-08 00:25:17 +0530

    fix mail config

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -2,15 +2,12 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: parseInt(process.env.EMAIL_PORT) || 587,
-  secure: process.env.EMAIL_PORT == 465,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false // Helps with some certification issues
-  }
 });
 
 // Verify connection on startup
